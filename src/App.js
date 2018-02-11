9b71e3a423281f6da5c87590111d20a38dd67fd5
Matthew Mogavero <matt.moga@gmail.com>
2018-02-11 15:59:06 -0500

    added functionality to click between X or O

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
-import Boxes from './Boxes';
+import Box from './Box';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      question: '',
-      answer: ''
+      boxes: [
+        {
+          isXO: true,
+        },
+        {
+          isXO: false,
+        },
+        {
+          isXO: false,
+        },
+        {
+          isXO: true,
+        },
+        {
+          isXO: true,
+        },
+        {
+          isXO: true,
+        },
+        {
+          isXO: false,
+        },
+        {
+          isXO: true,
+        },
+        {
+          isXO: true,
+        },
+      ]
     }
+    this.handleChoice.bind(this);
+  }
+  handleChoice(position) {
+    const clickedBox = this.state.boxes[position];
+    const first = this.state.boxes.slice(0, position);
+    const last = this.state.boxes.slice(position + 1);
+    const newBoxes = [
+      ...first,
+      {...clickedBox, isXO: !clickedBox.isXO},
+      ...last
+    ];
+    this.setState({'boxes': newBoxes});
+    console.log(clickedBox);
   }
   render() {
+    const { boxes } = this.state;
+    const BoxGrid = boxes.map((box, index) => <Box key={ index }
+                                                  isXO = {box.isXO}
+                                      onChoice={() => this.handleChoice(index)} />)
     return (
       <div className="container">
+        <h1>Tic-Tac-Toe</h1>
              <div className="row">
-                <h1>Tic-Tac-Toe</h1>
+              { BoxGrid }
             </div>
-            <Boxes />
-            </div>
+      </div>
     );
   }
 }
---
+++ src/Box.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+render() {
+    const symbol = this.props.isXO ? 'X' : 'O';
+    return (
+        <div className="col-md-4">
+            <h1 onClick={this.props.onChoice}>{ symbol }</h1>
+        </div>
+        );
+    }
+}
+
+
+  export default Box;
