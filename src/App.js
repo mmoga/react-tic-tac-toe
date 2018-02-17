2c2dba639880939bbdd39ce291050d32908b5f1c
Matthew Mogavero <matt.moga@gmail.com>
2018-02-16 20:30:44 -0500

    added some styling to the index.css page. Current player toggle works but the currentSymbol aspect still does not

--- src/App.js
+++ src/App.js
@@ -5,48 +5,78 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-     currentPlayer: 'X',
+     currentPlayer: 'Ex',
       boxes: [
         {
           isX: true,
           powerOf: 0,
+          isClicked: 0,
+          playerPiece : ''
         },
         {
           isX: true,
-          powerOf: 1
+          powerOf: 1,
+          isClicked: 0,
+          playerPiece : ''
         },
         {
           isX: true,
-          powerOf: 2
+          powerOf: 2,
+          isClicked: 0,
+          playerPiece : ''
         },
         {
           isX: true,
-          powerOf: 3
+          powerOf: 3,
+          isClicked: 0,
+          playerPiece : ''
         },
         {
           isX: true,
-          powerOf: 4
+          powerOf: 4,
+          isClicked: 0,
+          playerPiece : ''
         },
         {
           isX: true,
-          powerOf: 5
+          powerOf: 5,
+          isClicked: 0,
+          playerPiece : ''
         },
         {
           isX: true,
-          powerOf: 6
+          powerOf: 6,
+          isClicked: 0,
+          playerPiece : ''
         },
         {
           isX: true,
-          powerOf: 7
+          powerOf: 7,
+          isClicked: 0,
+          playerPiece : ''
         },
         {
           isX: true,
-          powerOf: 8
+          powerOf: 8,
+          isClicked: 0,
+          playerPiece : ''
         },
       ]
     }
     this.handleChoice.bind(this);
+    // this.toggleCurrentPlayer.bind(this);
+    this.updateClick.bind(this);
+    // this.togglePlayerPiece.bind(this);
   }
+  updateClick() {
+    if (this.isClicked === 0) {
+      this.setState({isClicked: 1});
+      console.log(this.isClicked);
+    } 
+  }
+  // togglePlayerPiece() {
+    
+  // }
   handleChoice(position) {
     const clickedBox = this.state.boxes[position];
     const first = this.state.boxes.slice(0, position);
@@ -56,20 +86,32 @@ class App extends Component {
       {...clickedBox, isX: !clickedBox.isX},
       ...last
     ];
+    // toggles current player status
+    if (this.state.currentPlayer === 'Ex') {
+      this.setState({currentPlayer: 'Oh'});
+    } else {
+      this.setState({currentPlayer: 'Ex'});
+    }
+    this.setState({})
     this.setState({boxes: newBoxes});
     console.log(clickedBox);
-    this.setState({currentPlayer: 'O'});
+    // this.setState({currentPlayer: 'O'});
+    // this.toggleCurrentPlayer();
+    this.updateClick();
+    // this.togglePlayer();
   }
   render() {
     const { boxes } = this.state;
     const BoxGrid = boxes.map((box, index) => <Box key={ index }
                                                    isX = {box.isX}
+                                                   isClicked = {box.isClicked}
+                                                   currentPlayer = {this.state.currentPlayer}
                                                    onChoice={() => this.handleChoice(index)} />)
     return (
       <div className="container">
-        <h1>The Working Man's Tic-Tac-Toe</h1>
+        <h1>The Working Man's Noughts and Crosses</h1>
           <p>Just click a symbol and it'll change&mdash;nothin' fancy</p>
-        
+          <p className="currentPara">Current player is: { this.state.currentPlayer }</p>        
              <div className="d-flex justify-content-center">
                <div className="grid">
                 { BoxGrid }
--- src/Box.js
+++ src/Box.js
@@ -3,18 +3,21 @@ import React, { Component } from 'react';
 
 class Box extends Component {
 render() {
-    // const symbol = function() {
-    //     if (this.props.isX == true) {
-    //         return 'Ex';
-    //     } else {
-    //         return 'Oh';
+     // toggleCurrentSymbol() {
+    //     let currentSymbol;
+    //     if(this.state.currentPlayer === 'X') {
+    //         currentSymbol = 'Ex';
     //     }
-    // };
+    //         currentSymbol = 'Oh';
+    // }
     const symbol = this.props.isX ? 'Ex' : 'Oh';
-    return (
-        
-            <div className="box d-flex align-items-center justify-content-center" onClick={this.props.onChoice}>
-            <h1 className="symbol">{ symbol }</h1>
+
+    return (  
+            <div>
+                <div className="box d-flex align-items-center justify-content-center" 
+                onClick={this.props.onChoice}>
+                <h1 className="symbol">{ symbol }</h1>
+                </div>
             </div>
             
         );
